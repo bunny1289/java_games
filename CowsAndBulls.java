b05c379020e9671472566df02516618f93abed8a
Abhilash D <abhilashd@ideyalabs.com>
2024-06-03 16:24:04 +0530

    final2

--- CowsAndBulls.java
+++ CowsAndBulls.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class CowsAndBulls {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("--------------------------------Rules------------------------------------");
         System.out.println("Welcome to the Cows and Bulls Game!");
         System.out.println("1. The computer will choose a random 4-digit number with no repeating digits and no zeroes.");
@@ -9,14 +12,15 @@ public class CowsAndBulls {
         System.out.println("5. A 'bull' means you have guessed a correct digit in the correct position.");
         System.out.println("6. The game continues until you guess the number or decide to quit.");
         System.out.println("7. Good luck and have fun playing!");
-        System.out.println("--------------------------------------------------------------------------\n\n");
+        System.out.println("--------------------------------------------------------------------------\n");
         Randomguess obj = new Randomguess();
         int[] ans = obj.number();
+        int c=0;
         while (true) {
             int bulls = 0;
             int cows = 0;
-            userguess obj1 = new userguess();
-            int[] guess = obj1.guess();
+            UserGuess obj1 = new UserGuess();
+            int[] guess = obj1.guess(sc);
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 4; j++) {
                     if (guess[i] == ans[j]) {
@@ -29,13 +33,20 @@ public class CowsAndBulls {
                 }
 
             }
+            c++;
             if (bulls==4){
-                System.out.println("Your guess is correct!!!");
+                System.out.println("-------------------------");
+                System.out.println("Your guessed the number in "+c+" chances!!!");
+                System.out.println("-------------------------");
                 break;
             }
+            System.out.println("---------");
             System.out.println("Cows = "+cows + "\nBulls ="+ bulls);
+            System.out.println("---------");
+
 
         }
+        sc.close();
     }
 
 }
--- userguess.java
+++ userguess.java
@@ -1,61 +1,38 @@
 import java.util.Scanner;
 
-public class userguess {
+public class UserGuess {
 
-    public int[] guess() {
-        int[] list = new int[4];
-        try (Scanner obj = new Scanner(System.in)) {
-            while (true) {
-                boolean test = true;
-                System.out.print("Enter your guess : ");
-                int x = obj.nextInt();
-                obj.nextLine();String temp = x + "";
-                if (temp.length() > 4) {
-                    test = true;
-                } else {
-                    for (int i = 0; i < 4; i++) {
-                        int y = x % 10;
-                        x = x / 10;
-                        list[3 - i] = y;
-                    }
-
-                    for (int i = 0; i < 4; i++) {
-                        if (list[i] == 0) {
-                            test = true;
-                            break;
-                        }
-                        for (int j = i + 1; j < 4; j++) {
-                            if (list[i] == list[j]) {
-                                test = true;
-                                break;
-                            } else {
-                                test = false;
-                            }
-                        }
-                        if (test)
-                            break;
+    public int[] guess(Scanner sc){
+        int[] num = new int[4];
+        System.out.print("Enter your guess :");
+        // Scanner sc = new Scanner(System.in);
+        String temp = sc.nextLine();
+        if(temp.length() > 4){
+            System.out.println("*****Invalid Input!!!*****");
+            return guess(sc);
+        } else{
+            int n = Integer.parseInt(temp);
+            for(int i=0;i<4;i++){
+                num[3-i] = n%10;
+                n=n/10;
+            }
+            for(int i=0;i<4;i++){
+                if(num[i] == 0){
+                    System.out.println("---------------------------");
+                    System.out.println("*****Invalid Input!!!*****");
+                    System.out.println("---------------------------");
+                    return guess(sc);
+                }
+                for(int j=i+1;j<4;j++){
+                    if(num[i] == num[j]){
+                        System.out.println("---------------------------");
+                        System.out.println("*****Invalid Input!!!*****");
+                        System.out.println("---------------------------");
+                        return guess(sc);
                     }
                 }
-                if (!test)
-                    break;
-                else
-                    System.out.println("Your guess is invalid!!!");
             }
         }
-        int r = list[0];
-        for (int i = 1; i < 4; i++) {
-            r = r * 10 + list[i];
-        }
-        return list;
+        return num;
     }
-
-    // public static void main(String[] args) {
-    // userguess obj = new userguess();
-    // int [] res = obj.guess();
-    // int x = res[0];
-    // for (int i = 1; i < 4; i++) {
-    // x = x*10 + res[i];
-    // }
-    // System.out.println(x);
-    // }
 }
